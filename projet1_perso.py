86e064dd010bfd8c23e2ff1b68d2a29ce7c31489
coyote95 <marie.guillot.oc@gmail.com>
2023-08-11 14:09:35 +0200

    creation du fichier csv avec header

--- projet1_perso.py
+++ projet1_perso.py
@@ -1,3 +1,5 @@
+import csv
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -15,7 +17,26 @@ def save_page_html(soup,fichier):
         return
 
 def title(soup):
-    return soup.title.string
+    return soup.title.string.strip("\n")
+
+def fichier_csv(soup):
+    titre_fichier="livre1"
+    with open(f"{titre_fichier}.csv","w",newline="") as fichier:
+        fieldnames=["product_page_url","universal_product_code(upc)","title","price_including_tax","price_excluding_tax","number_available","product_description","category","review_rating","img_url"]
+        writer=csv.DictWriter(fichier,fieldnames=fieldnames)
+        writer.writeheader()
+        return
+
+def data(soup,produit):
+    th=[]
+    td=[]
+    for table in soup.find_all('table'):
+        for tr in table.find_all("tr"):
+            th.append(tr.find("th").get_text())  # colonne 1 avec header
+            td.append(tr.find("td").get_text())  # colonne 2 avec valeur
+    print(th)
+    print(td)
+
 
 def main():
     html_home=extract_data(URL_home)
@@ -25,6 +46,12 @@ def main():
     print(title(soup_home))
     save_page_html(soup_home,"page_home_html.txt")
     save_page_html(soup_livre, "page_livre.txt")
+
+    fichier_csv(soup_livre)
+
+    produit=[]
+
+    data(soup_livre,produit)
 
     return
 
